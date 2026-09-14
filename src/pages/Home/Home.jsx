0b1549c252aafc97fc82ffdac19ff01979eaacde
ibrahim Abdulmuiz <iabdulmuiz5@gmail.com>
2026-09-14 11:00:29 +0100

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Navbar/Header/Header'
+import Exploremenu from '../../components/Navbar/ExploreMenu/Exploremenu'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Exploremenu/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Placeholder/Placeholder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Placeholder.css'
+
+const Placeholder = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Placeholder
